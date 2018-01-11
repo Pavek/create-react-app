9805858d4e527aabf335eff736345fcb42c06d51
Pavel Zhytko <pavel@karambasecurity.com>
2018-01-11 19:53:34 +0300

    fix: scss config

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -182,13 +182,13 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.scss$/,
             use: [
               require.resolve('style-loader'),
               {
                 loader: require.resolve('css-loader'),
                 options: {
-                  importLoaders: 1,
+                  importLoaders: 2,
                 },
               },
               {
@@ -211,6 +211,8 @@ module.exports = {
                   ],
                 },
               },
+              // Pavek: add SCSS support
+              require.resolve('sass-loader'),
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
